580072eec744cb7754e43d3e220b725572957f01
Shon Dsouza <shondsouza11@gmail.com>
2025-02-06 23:03:57 +0530

    initial commit

--- Login/script.js
+++ Login/script.js
@@ -1,20 +1,14 @@
-const wrapper = document.querySelector('.wrapper');
-const loginLink = document.querySelector('.login-link');
-const registerLink = document.querySelector('.register-link');
-const bthPopup = document.querySelector('.bthLogin-popup');
-const iconClose = document.querySelector('.icon-close');
-
-registerLink.addEventListener('click', () => wrapper.classList.add('active'));
-loginLink.addEventListener('click', () => wrapper.classList.remove('active'));
-bthPopup.addEventListener('click', () => wrapper.classList.add('active-popup'));
-iconClose.addEventListener('click', () => wrapper.classList.remove('active-popup'));
-// Show the quote box on page load
-window.onload = function () {
-    const quoteBox = document.getElementById("quoteBox");
-    quoteBox.style.display = "block"; // Show the box
-
-    // Hide it after 5 seconds
-    setTimeout(function () {
-        quoteBox.style.display = "none";
-    }, 20000); 
-};
+function validatePasswords() {
+    const password = document.querySelector('input[name="password"]').value;
+    const confirmPassword = document.querySelector('input[name="confirm-password"]').value;
+    const errorElement = document.getElementById('error');
+  
+    if (password !== confirmPassword) {
+      errorElement.textContent = "Passwords do not match!";
+      return false;
+    }
+  
+    errorElement.textContent = "";
+    return true;
+  }
+  
