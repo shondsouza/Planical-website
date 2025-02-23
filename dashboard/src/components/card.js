81349807fbf3687c5557781ac43561e6f010b926
Shon Dsouza <shondsouza11@gmail.com>
2025-02-23 23:25:16 +0530

    card.js

--- dashboard/src/components/card.js
+++ dashboard/src/components/card.js
@@ -1,38 +1,60 @@
-import React from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
+import './card.css';
 
-import PropTypes from 'prop-types'
-
-import './card.css'
-
-const Card = (props) => {
-  return (
-    <section className={`card-card ${props.rootClassName} `}>
-      <div className="card-icon1">
-        <img alt={props.iconAlt} src={props.icon} className="card-icon2" />
-      </div>
-      <main className="card-content">
-        <h1 className="card-header">{props.header}</h1>
-        <p className="card-description">{props.description}</p>
-      </main>
-    </section>
-  )
-}
+const Card = ({
+                  header,
+                  description,
+                  icon,
+                  iconAlt,
+                  rootClassName = '',
+              }) => {
+    return (
+        <section className={`card-card ${rootClassName}`.trim()}>
+            <div className="card-icon1">
+                <img
+                    alt={iconAlt}
+                    src={icon}
+                    className="card-icon2"
+                    loading="lazy"
+                />
+            </div>
+            <main className="card-content">
+                <h1 className="card-header">{header}</h1>
+                <p className="card-description">
+                    {typeof description === 'string'
+                        ? description
+                        : `${description.title}\n${description.specialization}`
+                    }
+                </p>
+            </main>
+        </section>
+    );
+};
 
 Card.defaultProps = {
-  description:
-    'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium',
-  rootClassName: '',
-  icon: '/Icons/group 1316.png',
-  iconAlt: 'image',
-  header: 'Sima Mosbacher',
-}
+    description: {
+        title: 'Licensed Clinical Psychologist',
+        specialization: 'Specializing in anxiety, depression, and trauma recovery with 10+ years of experience in cognitive behavioral therapy.'
+    },
+    rootClassName: '',
+    icon: '/Icons/group 1316.png',
+    iconAlt: 'Provider specialty icon',
+    header: 'Dr. Sarah Chen, PhD',
+};
 
 Card.propTypes = {
-  description: PropTypes.string,
-  rootClassName: PropTypes.string,
-  icon: PropTypes.string,
-  iconAlt: PropTypes.string,
-  header: PropTypes.string,
-}
+    description: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.shape({
+            title: PropTypes.string.isRequired,
+            specialization: PropTypes.string.isRequired
+        })
+    ]),
+    rootClassName: PropTypes.string,
+    icon: PropTypes.string.isRequired,
+    iconAlt: PropTypes.string.isRequired,
+    header: PropTypes.string.isRequired,
+};
 
-export default Card
+export default Card;
